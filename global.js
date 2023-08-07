4ef046c133428ff8ac369c414f9bb064ae192eac
Jiří Zmrhal <jiri.zmrhal@actumdigital.com>
2023-08-07 10:58:18 +0200

    Add a temporary hash change style fix

--- global.js
+++ global.js
@@ -1,7 +1,8 @@
-// import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
-// // Issue: CSS :target selector does not work with client-side routing
-// // Hack: Update the URL hash on each route update
+// Hack v1: Does not always work
+// Issue: CSS :target selector does not work with client-side routing
+// Hack: Update the URL hash on each route update
 // export function onRouteUpdate({location, previousLocation}) {
 //   if (location.pathname !== previousLocation?.pathname) {
 //     if (ExecutionEnvironment.canUseDOM && window.location.hash) {
@@ -15,3 +16,34 @@
 //   }
 //   return undefined;
 // }
+
+// Hack v2
+// Issue: CSS :target selector does not work with client-side routing
+// Hack: Add a class to the element with the hash ID until Docusaurus fixes the issue
+
+function handleHashChange() {
+  if (ExecutionEnvironment.canUseDOM && window.location.hash) {
+    const hash = window.location.hash.substring(1)
+    const targetElement = document.getElementById(hash);
+    const targetClassName = "url-target"
+
+    document.querySelectorAll(`.${targetClassName}`).forEach((el) => el.classList.remove(targetClassName))
+
+    if (targetElement) {
+      targetElement.classList.add(targetClassName);
+    }
+  }
+}
+
+export function onRouteDidUpdate({location, previousLocation}) {
+  if (location.pathname !== previousLocation?.pathname) {
+    if (ExecutionEnvironment.canUseDOM && window.location.hash) {
+      handleHashChange()
+    }
+  }
+  return undefined;
+}
+
+window.addEventListener("hashchange", () => {
+  handleHashChange()
+});
